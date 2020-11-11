7a1d2ba7c986d2e157c1ae51125af3da916b83fb
JoseRafael <amjrdesarrollo@gmail.com>
2020-11-10 18:37:04 -0600

    Primer Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
-const nombre = 'Rafael!!!!'
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
 
-saludar(nombre);
+
+export const todoList = new TodoList();
+
+// const tarea = new Todo('Aprender JavaScript!!');
+// todoList.nuevoTodo(tarea);
+
+// console.log(todoList);
+
+// crearTodoHtml( tarea );
+
+// localStorage.setItem('nombre','Rafael');
+// sessionStorage.setItem('nombre','Rafael');
+
+// setTimeout( ()=>{
+//     localStorage.removeItem('nombre');
+// }, 2000);
+
+todoList.todos.forEach( crearTodoHtml );
+
+// const newTodo = new Todo('Aprender Java Script');
+// todoList.nuevoTodo(newTodo);
+
+console.log('todos', todoList.todos)
